16304c807fcc6428e27c70ad6dbf5db68cc265fe
0xglider <0xglider@proton.me>
2025-06-17 09:50:50 -0600

    chore: forge init

--- start-ganache.js
+++
@@ -1,3 +0,0 @@
-const { spawn } = require('child_process')
-const mnemonic = 'supreme banner leisure dinosaur fold text friend ivory vacant city room poet'
-spawn('npx', ['ganache-cli', '-p 8545', '-d', `-m ${mnemonic}`, '-l 9900000', '-e 1000', '-g 10000000'])
