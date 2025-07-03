0829fdbe113bccbe48a7c3687dca97826cade74b
0xglider <0xglider@proton.me>
2025-07-03 12:50:36 -0600

    LibAccessRoles

--- src/libraries/LibAccessRoles.sol
+++ src/libraries/LibAccessRoles.sol
@@ -10,19 +10,4 @@ library LibAccessRoles {
 
     /// @notice Administrative role for managing global protocol configuration.
     bytes32 public constant _ADMIN = keccak256("_ADMIN");
-
-    /// @notice Role allowed to mint EqualFi NFTs.
-    bytes32 public constant _NFT_MINTER = keccak256("_NFT_MINTER");
-
-    /// @notice Role allowed to operate lending and borrowing logic.
-    bytes32 public constant _LENDING_OPERATOR = keccak256("_LENDING_OPERATOR");
-
-    /// @notice Role authorized to perform liquidations.
-    bytes32 public constant _LIQUIDATOR = keccak256("_LIQUIDATOR");
-
-    /// @notice Role responsible for managing staking parameters and rewards.
-    bytes32 public constant _STAKING_MANAGER = keccak256("_STAKING");
-
-    /// @notice Role for backend relayers or frontend automation features.
-    bytes32 public constant _RELAYER = keccak256("_RELAYER");
 }
